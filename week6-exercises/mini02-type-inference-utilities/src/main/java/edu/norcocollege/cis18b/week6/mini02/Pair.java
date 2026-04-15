e44a0e03f06f0b8287e8728c4c64d86f5f681057
Daniel Rios <danieljrm100873@gmail.com>
2026-04-15 03:42:10 +0000

    Mini 02 is complete with added comments to describe the code's implementation

--- week6-exercises/mini02-type-inference-utilities/src/main/java/edu/norcocollege/cis18b/week6/mini02/Pair.java
+++ week6-exercises/mini02-type-inference-utilities/src/main/java/edu/norcocollege/cis18b/week6/mini02/Pair.java
@@ -1,5 +1,13 @@
 package edu.norcocollege.cis18b.week6.mini02;
 
+/*
+ * Here's a generic pair record that stores two related values
+ *
+ * @param left the left value
+ * @param right the right value
+ * @param <L> type of the left value
+ * @param <R> type of the right value
+ */
 public record Pair<L, R>(L left, R right) {
 
     @Override
--- week6-exercises/mini02-type-inference-utilities/src/main/java/edu/norcocollege/cis18b/week6/mini02/TypeInferenceUtilitiesDemo.java
+++ week6-exercises/mini02-type-inference-utilities/src/main/java/edu/norcocollege/cis18b/week6/mini02/TypeInferenceUtilitiesDemo.java
@@ -1,18 +1,45 @@
 package edu.norcocollege.cis18b.week6.mini02;
 
+/*
+ * Demonstrates generic helper methods and Java type inference
+*/
 public class TypeInferenceUtilitiesDemo {
 
+    /*
+     * Factory method that creates a Pair
+    */
     public static <L, R> Pair<L, R> pair(L left, R right) {
         return new Pair<>(left, right);
     }
 
+    /*
+     * Returns the second of two values of the same type
+    */
     public static <T> T chooseSecond(T first, T second) {
         return second;
     }
 
+    /*
+     * Extension challemge method implemented:
+     * Returns the first non-null value, or null if both are null
+     */
+    public static <T> T chooseNonNull(T first, T second) {
+        if (first != null) {
+            return first;
+        }
+        return second;
+    }
+
     public static void main(String[] args) {
+
+        // Type arguments are inferred automatically from the arguments here
         Pair<String, Integer> studentScore = pair("Ada", 98);
+
+        // Java infers T as String
         String secondName = chooseSecond("Ada", "Grace");
+
+        // Explicit type arguments are able to improve readability when null is involved,
+        // b/c they make the intended type clear to the reader
         String explicitValue = TypeInferenceUtilitiesDemo.<String>chooseSecond(null, "fallback");
 
         System.out.println("Pair: " + studentScore);
